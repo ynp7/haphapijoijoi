38c971918af023e0a85a21fd3d89e1e0175c23ab
Timothy Pew <mrlasers@mrlasers.com>
2017-07-01 12:52:47 -0700

    add hapi server

---
+++ index.js
@@ -0,0 +1,27 @@
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+server.connection({ port: 8080 })
+
+server.register( require('inert'), (err) => {
+  if (err) {
+    throw err
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.file('./public/index.html')
+    }
+  })
+})
+
+server.start( (err) => {
+  if (err) {
+    throw err
+  }
+
+
+  console.log(`Server running at ${server.info.uri}`)
+})
