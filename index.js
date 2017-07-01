c406a612e7d72fd907e434c8ba51b4381912f908
Timothy Pew <mrlasers@mrlasers.com>
2017-07-01 14:37:43 -0700

    remove inert

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 server.connection({ port: 8080 })
 
-server.register( require('inert'), (err) => {
-  if (err) {
-    throw err
-  }
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      return reply('Hello, World!')
-    }
-  })
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    return reply('Hello, World!')
+  }
 })
 
 server.start( (err) => {
