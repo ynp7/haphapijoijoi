34052279214a2b7dc4316c5fce50731cb41260e9
Timothy Pew <mrlasers@mrlasers.com>
2017-07-09 22:32:09 -0700

    add cross-env and react to package.json

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-const _join = require('lodash').join
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <title>Hello, React</title>
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>
 
-const steelz = require('./styles.less')
-
-console.log(process.env.NODE_ENV)
-
-const component = () => {
-  var element = document.createElement('div')
-  process.env.NODE_ENV === 'production'
-    ? element.innerHTML = _join(['===', 'PRODUCTION SERVER', '==='], ' ')
-    : element.innerHTML = _join(['===', 'DEVELOPMENT SERVER', '==='], ' ')
-  return element
-}
-
-console.log('Hello, Webpack! --bundled!')
-
-document.body.appendChild(component())
+// const _join = require('lodash').join
+//
+// const steelz = require('./styles.less')
+//
+// console.log(process.env.NODE_ENV)
+//
+// const component = () => {
+//   var element = document.createElement('div')
+//   process.env.NODE_ENV === 'production'
+//     ? element.innerHTML = _join(['===', 'PRODUCTION SERVER', '==='], ' ')
+//     : element.innerHTML = _join(['===', 'DEVELOPMENT SERVER', '==='], ' ')
+//   return element
+// }
+//
+// console.log('Hello, Webpack! --bundled!')
+//
+// document.body.appendChild(component())
 
 // document.body.appendChild( () => {
 //   var element = document.createElement('h1')
